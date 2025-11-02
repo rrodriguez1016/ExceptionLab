7950ee456a0c8420c3eb91838163f859e0274704
Rene Rodriguez <rodriguezr1016@gmail.com>
2025-11-02 14:49:40 -0800

    final commit

---
+++ build/CMakeFiles/ErrorHandling.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/rodri/projects/homework/fu/Exceptions.cpp" "CMakeFiles/ErrorHandling.dir/Exceptions.cpp.obj" "gcc" "CMakeFiles/ErrorHandling.dir/Exceptions.cpp.obj.d"
+  "C:/Users/rodri/projects/homework/fu/FileUtils.cpp" "CMakeFiles/ErrorHandling.dir/FileUtils.cpp.obj" "gcc" "CMakeFiles/ErrorHandling.dir/FileUtils.cpp.obj.d"
+  "C:/Users/rodri/projects/homework/fu/GeometryUtils.cpp" "CMakeFiles/ErrorHandling.dir/GeometryUtils.cpp.obj" "gcc" "CMakeFiles/ErrorHandling.dir/GeometryUtils.cpp.obj.d"
+  "C:/Users/rodri/projects/homework/fu/main.cpp" "CMakeFiles/ErrorHandling.dir/main.cpp.obj" "gcc" "CMakeFiles/ErrorHandling.dir/main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/ErrorHandling.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ErrorHandling.dir/Exceptions.cpp.obj"
+  "CMakeFiles/ErrorHandling.dir/Exceptions.cpp.obj.d"
+  "CMakeFiles/ErrorHandling.dir/FileUtils.cpp.obj"
+  "CMakeFiles/ErrorHandling.dir/FileUtils.cpp.obj.d"
+  "CMakeFiles/ErrorHandling.dir/GeometryUtils.cpp.obj"
+  "CMakeFiles/ErrorHandling.dir/GeometryUtils.cpp.obj.d"
+  "CMakeFiles/ErrorHandling.dir/main.cpp.obj"
+  "CMakeFiles/ErrorHandling.dir/main.cpp.obj.d"
+  "ErrorHandling.exe"
+  "ErrorHandling.exe.manifest"
+  "ErrorHandling.pdb"
+  "libErrorHandling.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ErrorHandling.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
